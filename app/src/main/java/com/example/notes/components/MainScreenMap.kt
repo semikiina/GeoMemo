886851f3c65961a7a0a2ccf9b12aec73aed15368
jachyman <55025878+jachyman@users.noreply.github.com>
2024-12-05 14:06:51 +0100

    Ask for location permission

--- app/src/main/java/com/example/notes/components/MainScreenMap.kt
+++ app/src/main/java/com/example/notes/components/MainScreenMap.kt
@@ -1,13 +1,17 @@
 package com.example.notes.components
 
-
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -20,6 +24,8 @@ import com.example.notes.utils.Screen
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.maps.android.compose.Circle
 
+private const val PERMISSION = "android.permission.ACCESS_FINE_LOCATION"
+
 @SuppressLint("InlinedApi")
 @Composable
 fun MainScreenMap(navController: NavController){
@@ -27,20 +33,39 @@ fun MainScreenMap(navController: NavController){
     val context = LocalContext.current
 
     val london = LatLng(51.5074, -0.1278)   // initial location
-
     val currentLocation = remember { mutableStateOf(london) }
-    LaunchedEffect(Unit) {
-        currentLocation.value = getCurrentLocation(context)
+
+    val granted = remember {
+        mutableStateOf(
+            PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, PERMISSION))
+        }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Log.i("Location", "Permission was granted")
+        } else {
+            Log.i("Location", "Permission was NOT granted")
+        }
+        granted.value = isGranted
     }
+
+    LaunchedEffect(Unit) {
+        if (!granted.value) {
+            Log.i("Location", "Permission isnt granted")
+            launcher.launch(PERMISSION)
+        }
+        if (granted.value) {
+            currentLocation.value = getCurrentLocation(context)
+        }
+    }
+
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(
             currentLocation.value, 15f
         )
     }
-
-    var circle = Circle(
-        center = currentLocation.value,
-        radius = 50.0)
 
     LaunchedEffect(key1 = currentLocation.value) {
         currentLocation.value.let {
@@ -55,7 +80,7 @@ fun MainScreenMap(navController: NavController){
     GoogleMap(
         cameraPositionState=cameraPositionState,
         properties = MapProperties(
-            isMyLocationEnabled = true
+            isMyLocationEnabled = granted.value
         ),
         onPOIClick = { poi ->
             // Handle POI click
